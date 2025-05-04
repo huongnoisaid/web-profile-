0d2fcd15c2050b242027d6bc5534ed99acff47e4
huongnoisaid <baokeo10085@gmail.com>
2025-05-04 15:24:13 +0700

    Update freeze.py

--- freeze.py
+++ freeze.py
@@ -1,7 +1,7 @@
 from flask_frozen import Freezer
 from app import app
 
-# Configure Frozen-Flask
+# Cấu hình Frozen-Flask
 app.config['FREEZER_DESTINATION'] = 'build'
 app.config['FREEZER_RELATIVE_URLS'] = True
 

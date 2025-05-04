1742c15d24290b10c69722a5424f3b097e10485a
huongnoisaid <baokeo10085@gmail.com>
2025-05-04 15:05:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-from flask import Flask, render_template, request
+from flask_frozen import Freezer
+from app import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/project')
-def project():
-    return render_template('project.html')
+freezer = Freezer(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    freezer.freeze()
